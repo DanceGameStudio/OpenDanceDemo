e18a61ad7690cffd5d2f3755419b6ce9d2ef15eb
munir <munirzohbi@gmail.com>
2023-02-14 12:33:51 +0100

    segmentation_mask und Beispiel hinzugefügt

--- PoseEstimation.py
+++ PoseEstimation.py
@@ -2,23 +2,33 @@ import mediapipe as mp
 import numpy as np
 import cv2
 
+
 class PoseDetector:
     def __init__(self, capture):
         self.capture = capture
         self.media_pose = mp.solutions.pose
         self.media_draw = mp.solutions.drawing_utils
         self.image = np.zeros((1280, 720, 3), np.uint8)
-        self.pose = self.media_pose.Pose()
-        self.results = self.pose.process(self.image)
+        self.pose = self.media_pose.Pose(static_image_mode=False,
+                                         model_complexity=1,
+                                         smooth_landmarks=True,
+                                         enable_segmentation=True,
+                                         min_tracking_confidence=0.5,
+                                         min_detection_confidence=0.5)
+        self.results = None
+        self.segmentation_mask = None
 
     def detect_pose(self):
         success, self.image = self.capture.read()
         if not success:
             print("Image Stream not available")
             return
-        self.image = cv2.flip(self.image, 1)  # Horizontalflip
-        self.results = self.pose.process(self.image)
+        image = cv2.cvtColor(self.image, cv2.COLOR_BGR2RGB)
+        self.results = self.pose.process(image)
+        self.segmentation_mask = self.results.segmentation_mask
 
     def draw_landmarks(self):
-        if self.results.pose_landmarks:
-            self.media_draw.draw_landmarks(self.image, self.results.pose_landmarks, self.media_pose.POSE_CONNECTIONS)
+        if self.results.pose_landmarks and self.image is not None:
+            self.media_draw.draw_landmarks(self.image,
+                                           self.results.pose_landmarks,
+                                           self.media_pose.POSE_CONNECTIONS)
--- main.py
+++ main.py
@@ -2,18 +2,22 @@ import cv2
 import PoseEstimation
 
 if __name__ == '__main__':
-    capture = cv2.VideoCapture(0)
+    capture = cv2.VideoCapture('Videos/Beispiel_01.mp4')
+    #capture = cv2.VideoCapture(0) # Cam
     pose_detector = PoseEstimation.PoseDetector(capture)
+    flip = False
 
     while True:
         pose_detector.detect_pose()
         pose_detector.draw_landmarks()
+        output = pose_detector.image
 
         # Show Image
-        if pose_detector.image is not None:
-            cv2.imshow('PoseDetector', pose_detector.image)
+        if output is not None:
+            if flip:
+                output = cv2.flip(output, 1)  # Horizontalflip
+            cv2.imshow('PoseDetector', output)
         if cv2.waitKey(1) == ord('q'):
             capture.release()
             cv2.destroyAllWindows()
             break
-
