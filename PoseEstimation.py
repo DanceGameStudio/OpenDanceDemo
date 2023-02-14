c70fb6a7fc5ae5ec57e022bf75a9bc2c9f5831b7
munir <munirzohbi@gmail.com>
2023-02-14 10:55:43 +0100

    PoseDetector Klasse definiert

--- PoseEstimation.py
+++ PoseEstimation.py
@@ -1,30 +1,24 @@
 import mediapipe as mp
+import numpy as np
 import cv2
 
-def start_camera_detection():
-    media_pose = mp.solutions.pose
-    media_draw = mp.solutions.drawing_utils
-    pose = media_pose.Pose()
-    capture = cv2.VideoCapture(0)
+class PoseDetector:
+    def __init__(self, capture):
+        self.capture = capture
+        self.media_pose = mp.solutions.pose
+        self.media_draw = mp.solutions.drawing_utils
+        self.image = np.zeros((1280, 720, 3), np.uint8)
+        self.pose = self.media_pose.Pose()
+        self.results = self.pose.process(self.image)
 
-    if not capture.isOpened():
-        print("Failed to open camera")
-        exit()
-
-    while True:
-        success, image = capture.read()
+    def detect_pose(self):
+        success, self.image = self.capture.read()
         if not success:
             print("Image Stream not available")
+            return
+        self.image = cv2.flip(self.image, 1)  # Horizontalflip
+        self.results = self.pose.process(self.image)
 
-        image = cv2.flip(image, 1)  # Horizontalflip
-        results = pose.process(image)
-
-        if results.pose_landmarks:
-            media_draw.draw_landmarks(image, results.pose_landmarks, media_pose.POSE_CONNECTIONS)
-
-        cv2.imshow('Camera', image)
-        if cv2.waitKey(1) == ord('q'):
-            break
-
-    capture.release()
-    cv2.destroyAllWindows()
+    def draw_landmarks(self):
+        if self.results.pose_landmarks:
+            self.media_draw.draw_landmarks(self.image, self.results.pose_landmarks, self.media_pose.POSE_CONNECTIONS)
--- main.py
+++ main.py
@@ -1,8 +1,19 @@
-import VideoCapture as vc
-import PoseEstimation as pe
+import cv2
+import PoseEstimation
 
 if __name__ == '__main__':
-    #vc.start_camera_capture()
-    pe.start_camera_detection()
+    capture = cv2.VideoCapture(0)
+    pose_detector = PoseEstimation.PoseDetector(capture)
 
+    while True:
+        pose_detector.detect_pose()
+        pose_detector.draw_landmarks()
+
+        # Show Image
+        if pose_detector.image is not None:
+            cv2.imshow('PoseDetector', pose_detector.image)
+        if cv2.waitKey(1) == ord('q'):
+            capture.release()
+            cv2.destroyAllWindows()
+            break
 
